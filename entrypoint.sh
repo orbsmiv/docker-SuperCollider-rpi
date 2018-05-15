ae445e9ddbf4d68d6180a9f56a813df08c0c3207
James Carpenter <j.j.carpenter@bham.ac.uk>
2018-05-15 15:47:37 +0100

    disable jack in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # /usr/local/bin/jackd -P75 -dalsa -dhw:1 -p1024 -n3 -s -r44100
-/usr/local/bin/jackd -r -d alsa -d hw:0 -p 1024 -n 3 -s -r 48000 -P
+# /usr/local/bin/jackd -m -r -d alsa -d hw:0 -p 1024 -n 3 -s -r 48000 -P
 
 
 # exec /usr/local/bin/sclang "$@"
