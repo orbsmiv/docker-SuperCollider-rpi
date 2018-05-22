a3067b00a3b4247732bb501180a349b57e6b2341
James Carpenter <jamesjcarpenter@gmail.com>
2018-05-22 22:37:43 +0100

    use POSIX comparison

--- jack-connector.sh
+++ jack-connector.sh
@@ -9,13 +9,13 @@ while ! /usr/local/bin/jack_lsp | /bin/grep -F "SuperCollider" > /dev/null; do
   /bin/sleep 1
 done
 
-if ((${CH_OUT} > 0)); then
+if [ "${CH_OUT}" -gt "0" ]; then
   for i in $(seq 1 ${CH_OUT}); do
     $CONN_PATH SuperCollider:out_$i system:playback_$i ;
   done;
 fi
 
-if ((${CH_IN} > 0)); then
+if [ "${CH_IN}" -gt "0" ]; then
   for i in $(seq 1 ${CH_IN}); do
     $CONN_PATH SuperCollider:in_$i system:capture_$i ;
   done;
