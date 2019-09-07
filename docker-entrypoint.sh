23794e891be88676d4d57c0329615d6988a214fd
James Carpenter <orbsmiv@hotmail.com>
2019-09-08 00:32:36 +0100

    su-exec to non-root user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 # Define the jackd server options - see https://github.com/jackaudio/jackaudio.github.com/wiki/jackdrc(5)
 echo "/usr/bin/jackd -m -R -p 32 -T -d alsa -d ${ALSA_DEV} \"-n 3\" \"-i ${CH_IN}\" \"-o ${CH_OUT}\" \"-p ${HW_BUFF}\" \"-r ${SR}\" -P -s" > /etc/jackdrc
 
-exec "$@"
+exec su-exec sc-user "$@"
