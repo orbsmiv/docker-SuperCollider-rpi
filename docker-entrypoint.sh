bedbb00ca181e9593cdc5086b9233e029b302e03
James Carpenter <orbsmiv@hotmail.com>
2019-10-27 00:29:55 +0100

    more logic to entrypoint jackdrc builder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env ash
 
 # Define the jackd server options - see https://github.com/jackaudio/jackaudio.github.com/wiki/jackdrc(5)
-echo "/usr/bin/jackd -m -R -p 32 -T -d alsa -d ${ALSA_DEV} \"-n 3\" \"-i ${CH_IN}\" \"-o ${CH_OUT}\" \"-p ${HW_BUFF}\" \"-r ${SR}\" -P -s" > ${HOME}/.jackdrc
+BASE_OPTIONS="/usr/bin/jackd -m -R -p 32 -T --driver alsa"
+
+ALSA_OPTIONS="--device ${ALSA_DEV} \"--nperiods 3\" \"-i ${CH_IN}\" \"-o ${CH_OUT}\" \"--period ${HW_BUFF}\" \"--rate ${SR}\" --softmode"
+
+if [ "${SHORTS}" = "true" ]; then
+    ALSA_OPTIONS="${ALSA_OPTIONS} --shorts"
+fi
+
+if [ "${CH_IN}" -lt "1" ]; then
+    ALSA_OPTIONS="${ALSA_OPTIONS} --playback"
+fi
+
+if [ "${CH_OUT}" -lt "1" ]; then
+    ALSA_OPTIONS="${ALSA_OPTIONS} --capture"
+fi
+
+echo "${BASE_OPTIONS}" "${ALSA_OPTIONS}" > "${HOME}"/.jackdrc
 
 exec "$@"
