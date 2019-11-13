f1f26fdf53dd06b4d764affc794d8ad0be5b8a34
James Carpenter <orbsmiv@hotmail.com>
2019-11-13 23:30:51 +0000

    make entrypoint jackd command more explicit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env ash
 
 # Define the jackd server options - see https://github.com/jackaudio/jackaudio.github.com/wiki/jackdrc(5)
-BASE_OPTIONS="/usr/bin/jackd -m -R -p 32 -T --driver alsa"
+BASE_OPTIONS="/usr/bin/jackd -m -R -P 99 -p 32 -T -d alsa"
 
-ALSA_OPTIONS="--device ${ALSA_DEV} \"--nperiods 3\" \"-i ${CH_IN}\" \"-o ${CH_OUT}\" \"--period ${HW_BUFF}\" \"--rate ${SR}\" --softmode"
+ALSA_OPTIONS="--device=${ALSA_DEV} --nperiods=3 --inchannels=${CH_IN} --outchannels=${CH_OUT} --period=${HW_BUFF} --rate=${SR} --softmode"
 
 if [ "${SHORTS}" = "true" ]; then
     ALSA_OPTIONS="${ALSA_OPTIONS} --shorts"
